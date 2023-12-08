c22e8f27b4434c1fd22cdf2071fd66d88717291e
BharatiOm007 <153294834+BharatiOm007@users.noreply.github.com>
2023-12-08 17:05:19 +0000

    datatypes

---
+++ 01_basic/01_datatypes.js
@@ -0,0 +1,35 @@
+"use strict" // traet all js code as newer version
+
+//alert (3+3) we are using node js not a browser
+
+console.log(3+
+    3); // code should be readble not this type 
+
+
+    console.log("Hitesah");
+
+    let name = "om"
+    let age = 18 
+    let islogin = false 
+
+
+
+ 
+// datatypes
+
+// number => 2 to power  of 53
+//bigint => 
+// String => 
+//booleab =>  true/false
+//undifined =>
+//symbol => unique 
+
+//object 
+
+console.log(typeof "hitesh");
+console.log(typeof age);
+console.log(typeof age);
+
+console.log(typeof null ); // datatypes are object  ****** interview 
+console.log(typeof undefined); // datatyped are undifined
+
---
+++ 01_basic/01_variables.js
@@ -0,0 +1,21 @@
+
+// below are the example for how to delclare a varibles 
+
+const accountId = 9604758665
+let email = "Om@gmail.com"
+let password = "121212"
+let name;
+accoutCity = "Jaypur"
+
+// prefer not use to var beacause of the block scopoe and functional scope 
+
+// in var varibales scope are not allowed thats why we use the let variables and in future use use let 
+
+
+// to prin in a cosole 
+console.log(accountId);
+
+
+// to print in a table format in a console
+console.table([accountId, email, password, name, accoutCity])
+

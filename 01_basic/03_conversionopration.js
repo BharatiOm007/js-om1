3f21fce82ce1aef2e1c0ce79835752364a4d392a
BharatiOm007 <153294834+BharatiOm007@users.noreply.github.com>
2023-12-08 18:10:40 +0000

    converion Oprarion

---
+++ 01_basic/03_conversionopration.js
@@ -0,0 +1,33 @@
+let  score = "om" ;
+
+console.log(typeof score);
+console.log(typeof(score));
+
+
+let valueInnumer = Number(score)
+ console.log( typeof valueInnumer);
+ console.log(  valueInnumer);
+
+
+ //33 => 33 
+ //"33abc" => non
+ // true => 1  // false => 0
+ //
+
+
+ let isloggedIn = ""
+
+ let bolleanislogginIn = Boolean(isloggedIn)
+
+ console.log(bolleanislogginIn)
+
+ // 1 convert into true 
+ // 0 convert into false 
+ // "" empty convert into false 
+ // " om " => true 
+
+ let somenumber = 33 
+
+ let convertnum = String(somenumber)
+  console.log(convertnum);
+  console.log(typeof convertnum)

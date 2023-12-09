a367474e2b078a7a0a14dc6dfbce6d10ad9d60b1
BharatiOm007 <153294834+BharatiOm007@users.noreply.github.com>
2023-12-09 05:55:14 +0000

    Opraters in JS

---
+++ 01_basic/04_Opraters.js
@@ -0,0 +1,28 @@
+// ********************* Opraters In Js ******************************
+
+let value = 3 
+ let negvalue = -value
+ console.log(negvalue);
+
+ // 2*2 
+ //2%2
+ //2+2
+ //2-2
+ //2=25
+ //2/2
+ //2**2
+
+
+ let str1 = "Om"
+ let str2 = " Bharati"
+
+ let str3 = str1 + str2
+  
+ console.log(str3);
+
+
+ console.log("1" + 5)
+
+ console.log("1" + "5")
+
+ console.log( 1 + 1 + "5")

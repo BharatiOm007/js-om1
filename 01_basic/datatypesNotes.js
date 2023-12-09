2207e8b0239cbd9a146af56dd7c8d61ef78e3b48
BharatiOm007 <153294834+BharatiOm007@users.noreply.github.com>
2023-12-09 17:49:41 +0000

    datatypesNotes

---
+++ 01_basic/datatypesNotes.js
@@ -0,0 +1,24 @@
+// datatypes 
+
+// primitive 7 types 
+// string , number , boolean , null , undifined , symbol  , bigint 
+
+const score = 100 
+const scorefull = 100.3 
+const isloggedIn = false
+const outsideTemp = null
+let userEmail ;
+
+
+// refrence ( Non Primitive )
+// array , object , function 
+
+const id =  Symbol("123")
+const anotherId = Symbol("123")
+
+console.log(id === anotherId);
+
+
+const bignumber = 2555653565656565n // this is a bigint number 
+
+

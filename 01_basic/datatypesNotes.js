9a001a46d6244743889ecfafdb9f86bfbfed7b19
BharatiOm007 <153294834+BharatiOm007@users.noreply.github.com>
2023-12-10 08:33:08 +0000

    datataypes new

--- 01_basic/datatypesNotes.js
+++ 01_basic/datatypesNotes.js
@@ -16,9 +16,42 @@ let userEmail ;
 const id =  Symbol("123")
 const anotherId = Symbol("123")
 
-console.log(id === anotherId);
+//console.log(id === anotherId);
 
 
 const bignumber = 2555653565656565n // this is a bigint number 
+
+
+// function greet(name) {
+//     console.log(`Hello, ${name}!`);
+//   }
+  
+
+//   greet('Alice');
+
+
+// function greet(name){
+//     console.log(`Hello ${name}`);
+// }
+
+// greet('Omkar')
+
+let currrentDate = new Date();
+
+//console.log(currrentDate)
+
+let person = {
+    name : 'Omkar',
+    age :25 ,
+    city : "Pune"
+};
+
+// console.log(person.name); 
+// console.log(person['age']);   // Output: 30
+// console.log(person.city); 
+
+console.log([person]);
+
+
 
 
